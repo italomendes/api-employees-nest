e84d2c6365d72b8f313c4c0ffa6743a1f167748c
Italo Mendes <italomemendes@gmail.com>
2022-03-02 12:24:31 -0300

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Employees')
+    .setDescription('Employees management')
+    .build();
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('v1/api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
