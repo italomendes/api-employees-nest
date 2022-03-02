5b16be6d9d3a4dd62e8612ab75dd2d110b567492
Italo Mendes <italomemendes@gmail.com>
2022-03-02 13:51:27 -0300

    feat: role crud implemented

--- src/modules/department/dto/update-department.dto.ts
+++ src/modules/department/dto/update-department.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateDepartmentDto } from './create-department.dto';
 
-export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {}
+export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  costCenterId: string;
+}
--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleRepository } from './role.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
   controllers: [RoleController],
   providers: [RoleService],
 })
